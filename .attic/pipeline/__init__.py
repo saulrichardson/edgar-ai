fee7317b3df0ee646517e5ba943241fb91fefb4d
saulrichardson <saulsido101@gmail.com>
2025-07-21 19:09:30 -0400

    Stop tracking .attic directory

--- .attic/pipeline/__init__.py
+++
@@ -1,6 +0,0 @@
-# Package for pipeline orchestration utilities (e.g., choose_schema, run_pipeline)
-from .choose_schema import choose_schema
-from .extractor import extract
-from .run_pipeline import run_pipeline
-
-__all__ = ["choose_schema", "extract", "run_pipeline"]
